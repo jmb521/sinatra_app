3f0bae03ddd5b38e837e2756c9b04ba1d05dd327
Jennifer Pazos <jmp521@gmail.com>
2019-04-11 20:19:33 -0500

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,28 @@ class ApplicationController < Sinatra::Base
   helpers do
     def logged_in?
       !!session[:user_id]
-      
+
     end
 
     def current_user
       @user = User.find(session[:user_id])
     end
+
+    # def partial(template, *args)
+    # options = args.extract_options!
+    # options.merge!(:layout => false)
+    # if collection = options.delete(:collection) then
+    #   collection.inject([]) do |buffer, member|
+    #     buffer << haml(template, options.merge(
+    #                               :layout => false,
+    #                               :locals => {template.to_sym => member}
+    #                             )
+    #                  )
+    #   end.join("\n")
+    # else
+    #   haml(template, options)
+    # end
+  # end
 
   end
 
